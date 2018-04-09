34b3fbd7fe36d92f1b8513af503efd0adc541c91
arichards558 <31348479+arichards558@users.noreply.github.com>
2018-04-09 13:01:05 -0500

    update

--- Cartwright Clerking/Cartwright Clerking/Source.cpp
+++ Cartwright Clerking/Cartwright Clerking/Source.cpp
@@ -7,6 +7,10 @@
 
 int main()
 {
+	double normalPay;
+	double overTimePay=0;
+	double hoursWorked;
+	double hourlyPay = 10.00;
 	double grossPay;
 	double FWT = 0.80;
 	double FICA = 0.08;
@@ -16,8 +20,24 @@ int main()
 
 	std::cout << "Welcome to weekly payroll, Please enter your employees name." << std::endl;
 	std::cin >> employeeName;
-	std::cout << "Now please insert how much your employees gross pay is." << std::endl;
-	std::cin >> grossPay;
+	std::cout << "Now please insert how many hours your employee has worked." << std::endl;
+	std::cin >> hoursWorked;
+	/*normalPay = hourlyPay * hoursWorked;*/
+	if (hoursWorked <= 40)
+	{
+		normalPay = hoursWorked * hourlyPay;
+
+	}
+	if(hoursWorked > 40)
+	 {
+		 overTimePay = (hourlyPay * 1.5)*(hoursWorked - 40);
+		 normalPay = hourlyPay * 40;
+		 grossPay = normalPay + overTimePay;
+		 
+	 }
+	 
+	
+	std::cout << " Your employee made " << grossPay << std::endl;
 	netPay = (grossPay * FWT) - (grossPay * FICA) - (grossPay * stateTax);
 	std::cout << "Okay your employee " << employeeName << " is going to have a net pay of " << netPay << " dollars." << std::endl;
 
